f56c29fcab1236cf9a8c726c09af68753c6d8c1a
Johnnygames <johnegames@gmail.com>
2015-02-27 15:27:29 -0800

    Decided on schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,11 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  'objectID' INTEGER NOT NULL auto_increment,
+  'username' VARCHAR(20) NOT NULL,
+  'roomname' VARCHAR(20) NOT NULL,
+  'text' VARCHAR(100) NOT NULL,
+  PRIMARY KEY ('objectID')
 );
 
 /* Create other tables and define schemas for them here! */
